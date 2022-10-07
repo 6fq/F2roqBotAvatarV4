e1fc89c3d32a54e5c32a47e825e9682252c6dab0
Oncl f2roq <97564562+6fq@users.noreply.github.com>
2022-10-07 22:28:32 +0200

    Update avatar-banner.js

--- commands/image/avatar-banner.js
+++ commands/image/avatar-banner.js
@@ -30,14 +30,14 @@ if(banner){
   .setColor(message.member.displayHexColor)
   .setDescription(`${user.tag}'s Banner`)
   .setImage(url) 
-message.channel.send(embed)
+message...({embeds : [embed]})
 } else {  
   if(accent_color) {
   const embed = new MessageEmbed()
   .setDescription(`${user.tag} doesn't have a banner but the do have a accent color`)
   .setImage(accent_color.url)
      .setColor(accent_color)
-   message.channel.send(embed)
+message...({embeds : [embed]})
   } else 
     return message.channel.send(`${user.tag} does not have a banner not a accent color!`)
 
